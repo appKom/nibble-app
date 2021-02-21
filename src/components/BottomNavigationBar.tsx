3fea734b70f889cc7fc7a7c0fb15711c7cda4bf7
jasminan <>
2021-02-21 17:24:01 +0100

    La til ikoner og sentret det

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { OnlineBlue } from "utils/colors";
+import { AiOutlinePlus } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { AiOutlineShoppingCart} from "react-icons/ai";
+
 
 type Props = {
   className?: string;
@@ -11,15 +15,15 @@ const BottomNavigationBar = ({ className }: Props) => {
   return (
     <nav className={className}>
       <LinkWrapper>
-        <StyledLink>
-          <Link to="/products">Products</Link>
-        </StyledLink>
-        <StyledLink>
-          <Link to="/profile">Profile</Link>
-        </StyledLink>
-        <StyledLink>
-          <Link to="/cart">Cart</Link>
-        </StyledLink>
+        <StyledListElement>
+          <StyledLink to="/products"> <AiOutlinePlus>  </AiOutlinePlus> <p>Products</p></StyledLink>
+        </StyledListElement>
+        <StyledListElement>
+          <StyledLink to="/profile"> <CgProfile> </CgProfile> Profile</StyledLink>
+        </StyledListElement>
+        <StyledListElement>
+          <StyledLink to="/cart"> <AiOutlineShoppingCart></AiOutlineShoppingCart> Cart</StyledLink>
+        </StyledListElement>
       </LinkWrapper>
     </nav>
   );
@@ -29,18 +33,32 @@ const LinkWrapper = styled.ol`
   padding: 0;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
 
-const StyledLink = styled.li`
+const StyledListElement = styled.li`
   list-style: none;
   a {
     text-decoration: none;
     color: #ffffff;
+    align: center;
+    
   }
+  
 `;
+
+const StyledLink = styled(Link)`
+display: flex;
+align-items: center;
+flex-flow: column;
+width: 33vw;
+`;
+
 
 export default styled(BottomNavigationBar)`
   background-color: ${OnlineBlue};
   padding: 10px 0;
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
 `;
