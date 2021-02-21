654c57f974b58447990ce870ee0e024cbfe4525c
Paal Larsen <paaledward@gmail.com>
2021-02-21 20:54:39 +0100

    Change redirect uri env variables

--- src/utils/oidc.ts
+++ src/utils/oidc.ts
@@ -2,7 +2,9 @@ import { UserManager } from "oidc-client";
 
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const authority = process.env.REACT_APP_OW_AUTHORITY;
-const redirect_uri = `${process.env.REACT_APP_AUTH_REDIRECT}/login`;
+const redirect_uri = `${
+  process.env.REACT_APP_URL || process.env.DEPLOY_URL
+}/login`;
 
 const userManager = new UserManager({
   client_id,
