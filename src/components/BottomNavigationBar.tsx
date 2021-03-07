5551b066701e7cbbc8773e55aae8d172ce30559e
Paalar <paaledward@gmail.com>
2021-03-07 15:02:12 +0100

    Change products icon to a menu icon

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { OnlineBlue } from "utils/colors";
-import { AiOutlinePlus } from "react-icons/ai";
+import { BiFoodMenu } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
-import { AiOutlineShoppingCart} from "react-icons/ai";
-
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 type Props = {
   className?: string;
@@ -16,13 +15,20 @@ const BottomNavigationBar = ({ className }: Props) => {
     <nav className={className}>
       <LinkWrapper>
         <StyledListElement>
-          <StyledLink to="/products"> <AiOutlinePlus>  </AiOutlinePlus> <p>Products</p></StyledLink>
+          <StyledLink to="/products">
+            <BiFoodMenu /> <p>Products</p>
+          </StyledLink>
         </StyledListElement>
         <StyledListElement>
-          <StyledLink to="/profile"> <CgProfile> </CgProfile> Profile</StyledLink>
+          <StyledLink to="/profile">
+            <CgProfile /> Profile
+          </StyledLink>
         </StyledListElement>
         <StyledListElement>
-          <StyledLink to="/cart"> <AiOutlineShoppingCart></AiOutlineShoppingCart> Cart</StyledLink>
+          <StyledLink to="/cart">
+            <AiOutlineShoppingCart />
+            Cart
+          </StyledLink>
         </StyledListElement>
       </LinkWrapper>
     </nav>
@@ -42,18 +48,15 @@ const StyledListElement = styled.li`
     text-decoration: none;
     color: #ffffff;
     align: center;
-    
   }
-  
 `;
 
 const StyledLink = styled(Link)`
-display: flex;
-align-items: center;
-flex-flow: column;
-width: 33vw;
+  display: flex;
+  align-items: center;
+  flex-flow: column;
+  width: 33vw;
 `;
-
 
 export default styled(BottomNavigationBar)`
   background-color: ${OnlineBlue};
