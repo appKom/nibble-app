0c2c3be86b7acd272be32574f806c7a13ec9bd9b
KC <kcesmena@gmail.com>
2021-03-21 15:59:49 +0100

    (#10) Remove and reduce item amount

--- src/state/cart.tsx
+++ src/state/cart.tsx
@@ -29,4 +29,29 @@ export const addCartItem = (cart: CartItem[], cartItem: CartItem) => {
   return [...filteredCart, mutatedCartItem];
 };
 
+export const reduceCartItem = (cart: CartItem[], cartItem: CartItem) => {
+  const cartItemIndex = cart.findIndex((item) => item.name === cartItem.name);
+  if (cartItemIndex === -1) {
+    return [...cart, cartItem];
+  }
+
+  const mutatedCartItem = {
+    ...cartItem,
+    quantity: cart[cartItemIndex].quantity - 1,
+  };
+
+  if (mutatedCartItem.quantity <= 0) {
+    // Noe bedre måte å bruke deleteCartItem?
+    const filteredCart = cart.filter((item) => item.name !== cartItem.name);
+    return [...filteredCart];
+  }
+  const filteredCart = cart.filter((item) => item.name !== cartItem.name);
+  return [...filteredCart, mutatedCartItem];
+};
+
+export const deleteCartItem = (cart: CartItem[], cartItem: CartItem) => {
+  const filteredCart = cart.filter((item) => item.name !== cartItem.name);
+  return [...filteredCart];
+};
+
 export default cartState;
