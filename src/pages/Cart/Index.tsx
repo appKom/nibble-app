3eca3064cc4314d3bfb393196132afc0af203696
Fredrik Skaug <fredskau@stud.ntnu.no>
2021-04-16 13:44:55 +0200

    Add and remove items from cart (#10)

---
+++ src/pages/Cart/Index.tsx
@@ -0,0 +1,33 @@
+import BottomNavigationBar from "components/BottomNavigationBar";
+import React from "react";
+import { useRecoilState } from "recoil";
+import cartState, {
+  useGetCartTotal
+} from "state/cart";
+import styled from "styled-components";
+import CartItem from "./Item";
+
+const Cart = () => {
+  // const [cart] = useRecoilState(cartState);
+  const cartTotal = useGetCartTotal();
+  const [cart, setCart] = useRecoilState(cartState);
+  //const pk = { name: "Powerking", price: 10, quantity: 1 };
+  console.log(cart);
+  const buy = () => {
+    console.log("kjøpt")
+  }
+  return (
+    <div>
+      {cart.map((item) => (
+        <CartItem cartItem={item}  key={item.name}/>
+      ))}
+      <p>Total: {cartTotal}kr</p>
+      {/* Pluss og minus burde kanskje bare vises ved siden av et produkt i cart hvis det finnes noe */}
+      <button onClick={buy}>Kjøp</button>
+      <BottomNavigationBar />
+    </div>
+  );
+};
+
+export default Cart;
+
