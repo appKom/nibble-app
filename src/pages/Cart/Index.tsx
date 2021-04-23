a7221ae579fb96c48d5815636ba48392cbb02a49
Keith Charlene Tan Esmena <kcesmena@stud.ntnu.no>
2021-04-23 13:17:58 +0200

    (#23) Added pop-up box

--- src/pages/Cart/Index.tsx
+++ src/pages/Cart/Index.tsx
@@ -1,33 +1,80 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import BottomNavigationBar from "components/BottomNavigationBar";
-import React from "react";
+import React, { useState } from "react";
 import { useRecoilState } from "recoil";
-import cartState, {
-  useGetCartTotal
-} from "state/cart";
-//import styled from "styled-components";
+import cartState, { emptyCart, useGetCartTotal } from "state/cart";
+import styled from "styled-components";
 import CartItem from "./Item";
 
 const Cart = () => {
-  // const [cart] = useRecoilState(cartState);
   const cartTotal = useGetCartTotal();
-  const [cart] = useRecoilState(cartState);
-  //const pk = { name: "Powerking", price: 10, quantity: 1 };
-  console.log(cart);
+  const [cart, setCart] = useRecoilState(cartState);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const buy = () => {
-    console.log("kjøpt")
+    setCart(emptyCart(cart));
+  };
+
+  function confirmation() {
+    setCart(emptyCart(cart));
+    onClose();
   }
+
   return (
     <div>
       {cart.map((item) => (
-        <CartItem cartItem={item}  key={item.name}/>
+        <CartItem cartItem={item} key={item.name} />
       ))}
       <p>Total: {cartTotal}kr</p>
-      {/* Pluss og minus burde kanskje bare vises ved siden av et produkt i cart hvis det finnes noe */}
-      <button onClick={buy}>Kjøp</button>
+      <button onClick={onOpen}>Kjøp</button>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xs">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirm purchase</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Total>
+              <p>
+                <strong>Your total</strong>
+              </p>
+              <p>{cartTotal}kr</p>
+            </Total>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={confirmation}>
+              Purchase
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
       <BottomNavigationBar />
     </div>
   );
 };
 
-export default Cart;
+const Total = styled.div`
+  width: 60%;
+  justify-content: center;
+  text-align: center;
+  background-color: #f7f7fb;
+  border-radius: 3px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 8em;
+  padding: 20px;
+`;
 
+export default Cart;
