0847414d168493173027d5fa7d493af858c5d66b
trymwk <54713587+trymwk@users.noreply.github.com>
2021-11-07 14:37:23 +0100

    (#11) Start on display of account holdings

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,29 @@
 import Page from "components/Page";
+import { useUser } from "state/auth";
+
+const getProfile = async (token:any) => {
+  const data = await fetch('https://online.ntnu.no/api/v1/profile/', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    return data;
+  });
+}
 
 const Profile = () => {
+  const user = useUser();
+  let data = getProfile(user?.access_token);
+  //console.log(data);
+  let saldo = data?.then(response => console.log(response));
   return (
     <Page>
-      <p>Profile</p>
+      <h1>{user?.profile.name}</h1>
+      <h1>{}</h1>
     </Page>
   );
 };
