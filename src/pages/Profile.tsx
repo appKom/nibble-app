8784a4405e174a5da28360e1af5c14f54bbf2827
Trym Wensberg Kulseng <trymwk@stud.ntnu.no>
2022-01-20 18:41:04 +0100

    (#11 #22) Beginning of profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,29 +1,36 @@
+import { Button, Heading, Stack } from "@chakra-ui/react";
+import { Image } from '@chakra-ui/react'
 import Page from "components/Page";
 import { useUser } from "state/auth";
-
-const getProfile = async (token:any) => {
-  const data = await fetch('https://online.ntnu.no/api/v1/profile/', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    return data;
-  });
-}
+import userManager from "utils/oidc";
+import GetUser from "./profile/GetUser";
 
 const Profile = () => {
   const user = useUser();
-  let data = getProfile(user?.access_token);
-  //console.log(data);
-  let saldo = data?.then(response => console.log(response));
+  const { data } = GetUser(`${process.env.REACT_APP_API_BASE}/profile/`);
+  const signoutRedirect = () => userManager.signoutRedirect();
   return (
     <Page>
-      <h1>{user?.profile.name}</h1>
-      <h1>{}</h1>
+      <Stack spacing={6} align={"center"}>
+        <Image
+          borderRadius='full'
+          boxSize='150px'
+          src={data.image}
+          alt={user?.profile.name}
+        />
+        <Heading as='h3' borderTop={"200px"}>
+          {user?.profile.name}
+        </Heading>
+        <Heading as='h3'>
+          Din saldo: {data.saldo} kr
+        </Heading>
+        <Button w="100%" onClick={() => console.log("Fyll på mer penger")}>
+          Fyll på mer penger
+        </Button>
+        <Button w="100%" onClick={signoutRedirect}>
+          Logg ut
+        </Button>
+      </Stack>
     </Page>
   );
 };
